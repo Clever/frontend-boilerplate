7fd922e6f1ddfe3a9dee46c9c9b159c9d30e4c81
Omar Diab <omar.diab@clever.com>
2017-03-30 17:23:29 -0700

    make whatwg-fetch a webpack include

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 // Actions define what events can cause application state to change. How the application state
 // actually changes is defined by each reducer in the Redux store (see the store/ directory).
 
-import 'whatwg-fetch';
-
 // Synchronous actions are just Javascript objects that contain a `type` and other metadata. The
 // `type` allows the `store` to distinguish what action is occurring, and, if present, the Redux
 // reducers can use the metadata in other fields to modify the state properly.
